f0e1b3be7cd0530d2dde303f998e2b718625de0c
Gaurav Jawalkar <gauravjawalkar8@gmail.com>
2025-11-09 23:56:17 +0530

    Add CORS support to handle cross-origin requests

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT!;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`))
+const allowedOrigins = [
+    'http://localhost:3000'
+]
 
 import cookieParser from 'cookie-parser';
 // Middlewares
 app.use(express.json());
 app.use(cookieParser())
+// production middleware for CORS policy : allow permission for cross origin requests
+app.use(cors({
+    origin: allowedOrigins
+}))
 
 // Specific controller imports
 import authRouter from './routes/auth.route'
